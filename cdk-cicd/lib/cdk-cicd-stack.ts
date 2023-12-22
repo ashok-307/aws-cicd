6aec7ea310d021244d89a50577500beaab941a13
Ashok <chakravarthi.ask@gmail.com>
2023-12-22 18:21:12 +0530

    Pipeline synth

--- cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk-cicd/lib/cdk-cicd-stack.ts
@@ -1,10 +1,37 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const pipeLine = new CodePipeline(this, 'AwesomePipeline', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('ashok-307/aws-cicd', 'cicd-practice'),
+        commands: [
+          'cd cdk-cicd',
+          'npm ci',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'cdk-cicd/cdk.out'
+      })
+    });
+
+    const testStage = pipeLine.addStage(new PipelineStage(this, 'PipelineTestStage', {
+      stageName: 'test'
+    }));
+
+    testStage.addPre(new CodeBuildStep('Unit-test', {
+      commands: [
+        'cd cdk-cicd',
+        'npm ci',
+        'npm test'
+      ]
+    }))
 
     // The code that defines your stack goes here
 
